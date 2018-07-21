eff6b39ceef371c9ab44af9c052cd9788cfb9f13
jdm79 <james.malvern@gmail.com>
2018-07-21 19:13:25 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# darts !
+# as a user so that we can start the game I am given 501 points
+#as a user, I want to throw 3 darts for each turn
+# as a user to win we need to reach strictly 0 from an even number up to 40# darts
---
+++ game2.rb
@@ -0,0 +1,35 @@
+class Game
+
+	def initialize(points = 501)
+		@points = points
+end
+
+	def throw
+		puts "'Throw' to throw your dart"
+		input = gets.chomp
+	end
+
+def threedart
+	counter = 0
+	# @points = 501
+	while @points <= 501 do
+		while counter < 3 do
+			throw
+			score = rand(1..60)
+			
+			puts "You got #{score}"
+			counter += 1 
+			if @points - score >= 0
+				@points = @points - score
+			else
+				@points
+			end
+			puts @points
+		end
+			counter = 0
+		end
+	end
+end
+
+game = Game.new
+game1 = game.threedart
